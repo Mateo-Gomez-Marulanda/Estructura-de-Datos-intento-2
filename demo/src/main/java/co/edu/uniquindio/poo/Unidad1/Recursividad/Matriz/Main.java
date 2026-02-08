008f3eb2482d93676e296eef6fbb4dca775e54ec
mateo <mateo.gomezm@uqvirtual.edu.co>
2026-02-07 21:59:20 -0500

    actividad para recorrer una matriz

--- demo/src/main/java/co/edu/uniquindio/poo/Unidad1/Recursividad/Matriz/Main.java
+++ demo/src/main/java/co/edu/uniquindio/poo/Unidad1/Recursividad/Matriz/Main.java
@@ -3,5 +3,37 @@ package co.edu.uniquindio.poo.Unidad1.Recursividad.Matriz;
 //matriz recursiva
 
 public class Main {
-    
+    public static void main(String[] args) {
+        int[][] matriz = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        recorrerMatriz(matriz, 0, 0);
+    }
+
+    /*
+     * el metodo para recorrer la matriz de manera recursiva consiste en recorrer la
+     * primera fila e imprimir cada columna de la fila, luego seguir con la fila
+     * siguiente y repetir el proceso
+     */
+
+    private static void recorrerMatriz(int[][] matriz, int fila, int columna) {
+
+        // caso base
+        if (matriz.length == fila) { // verifica que no se supere el numero de filas
+            return;
+        }
+
+        // caso recursivo
+        System.out.print(matriz[fila][columna] + " "); // imprime el elemento en la posicion especifica
+        if (columna < matriz[fila].length - 1) { // recorre cada columna de la fila
+            recorrerMatriz(matriz, fila, columna + 1);
+        } else {
+            System.out.println();
+            // una vez se recorrentodos las columnas de un fila se pasa a la siguientes
+            recorrerMatriz(matriz, fila + 1, 0);
+        }
+    }
 }
