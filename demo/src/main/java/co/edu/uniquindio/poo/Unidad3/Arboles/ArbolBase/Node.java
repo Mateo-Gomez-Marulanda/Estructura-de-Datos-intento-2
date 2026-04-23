9457efe442a77eacc4ac1e1a32bc2ff2d92f0dc4
Teo <mateo.gomezm@uqvirtual.edu.co>
2026-04-22 22:29:08 -0500

    creacion de arboles binario, queda pendiente hacer mejoras para que sean ABB y AVL

--- demo/src/main/java/co/edu/uniquindio/poo/Unidad3/Arboles/ArbolBase/Node.java
+++ demo/src/main/java/co/edu/uniquindio/poo/Unidad3/Arboles/ArbolBase/Node.java
@@ -1,11 +1,14 @@
 package co.edu.uniquindio.poo.Unidad3.Arboles.ArbolBase;
 
-public class Node<T> {
+public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
     private T value;
     private Node<T> left;
     private Node<T> right;
 
     public Node(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("El valor no puede ser nulo");
+        }
         this.value = value;
         this.left = null;
         this.right = null;
@@ -33,6 +36,21 @@ public class Node<T> {
 
     public void setRight(Node<T> right) {
         this.right = right;
+    }
+
+    @Override
+    public int compareTo(Node<T> other) {
+        if (other == null) {
+            throw new IllegalArgumentException("No se puede comparar con un nodo nulo");
+        }
+        return this.value.compareTo(other.value);
+
+        /*
+         * resultados:
+         * -1: this.value es menor que other.value
+         * 0: this.value es igual a other.value
+         * 1: this.value es mayor que other.value
+         */
     }
 
     @Override
--- demo/src/main/java/co/edu/uniquindio/poo/Unidad3/Arboles/ArbolBase/Tree.java
+++ demo/src/main/java/co/edu/uniquindio/poo/Unidad3/Arboles/ArbolBase/Tree.java
@@ -1,31 +1,92 @@
 package co.edu.uniquindio.poo.Unidad3.Arboles.ArbolBase;
 
-public class Tree<T> {
+public class Tree<T extends Comparable<T>> {
     private Node<T> root;
+    private int size;
 
     public Tree() {
         this.root = null;
+        this.size = 0;
     }
 
-    private int getHeight(Node<T> node) {
-        if (node == null) {
-            return 0;
-        }
-        return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
+    public int size() {
+        return size;
     }
 
-    // Metodos de recorrido por niveles: por niveles o amplitud
-    public void levelOrder() {
+    //verifica que el nodo actual tenga al menos un hijo
+    private boolean hasSon(Node<T> node) {
+        return node.getLeft() != null || node.getRight() != null;
+    }
+
+    public void put(T data) {
+        Node<T> newNodo = new Node<>(data);
         if (root == null) {
-            System.out.println("tree is empty.");
+            root = newNodo;
         } else {
-            levelOrderR(root);
-            System.out.println(); // salto de línea al final
+            putR(data, root);
         }
     }
 
-    private void levelOrderR(Node<T> current){
+    // metodo recursivo para agregar un nodo
+    private void putR(T data, Node<T> current) {
+
+        if (data.equals(current.getValue())) { // condicion para no aceptar repetidos
+            System.out.print("not added, the value already exists in the tree.");
+
+        } else if (data.compareTo(current.getValue()) < 0) {
+            // Si data es MENOR que current, va a la IZQUIERDA
+            if (current.getLeft() == null) {
+
+                Node<T> newNode = new Node<>(data);
+                current.setLeft(newNode); // se asigna el nuevo nodo a la izquierda del nodo actual
+                size++;
+            } else {
+                // lamado recursivo para seguir buscando el lugar correcto en el subarbol
+                // izquierdo
+                putR(data, current.getLeft());
+            }
+        } else {
+            // Si data es MAYOR que current, va a la DERECHA
+            if (current.getRight() == null) {
+                Node<T> newNode = new Node<>(data);
+                current.setRight(newNode); // se asigna el nuevo nodo a la derecha del nodo actual
+                size++;
+            } else {
+                // llamado recursivo para seguir buscando el lugar correcto en el subarbol
+                // derecho
+                putR(data, current.getRight());
+            }
+        }
+    }
+
+    public void remove(T data) {
         
+    }
+
+    public boolean binarySearch(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("The data cannot be null");
+        }
+        return binarySearchR(root, data); // se llama al metodo recursivo
+    }
+
+    // metodo recursivo para buscar un valor en el arbol
+    private boolean binarySearchR(Node<T> current, T data) {
+        if (current == null) {
+            return false;
+        }
+
+        // se guarda el valor de la comparacion con el fin de poder comparar el tamaño
+        // de los datos sin importar su tipo
+        int comparacion = current.getValue().compareTo(data);
+
+        if (comparacion == 0) {
+            return true; // Encontrado
+        } else if (comparacion > 0) {
+            return binarySearchR(current.getLeft(), data); // Izquierda: valores menores
+        } else {
+            return binarySearchR(current.getRight(), data); // Derecha: valores mayores
+        }
     }
 
     // Metodos de recorrido en profundidad: preorden, inorden y postorden
@@ -79,4 +140,26 @@ public class Tree<T> {
             System.out.print(current.getValue() + " "); // obtiene el valor del nodo actual
         }
     }
+
+    // pendiente de revision
+    // private int getHeight(Node<T> node) {
+    // if (node == null) {
+    // return 0;
+    // }
+    // return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
+    // }
+
+    // // Metodos de recorrido por niveles: por niveles o amplitud
+    // public void levelOrder() {
+    // if (root == null) {
+    // System.out.println("tree is empty.");
+    // } else {
+    // levelOrderR(root);
+    // System.out.println(); // salto de línea al final
+    // }
+    // }
+
+    // private void levelOrderR(Node<T> current){
+
+    // }
 }
